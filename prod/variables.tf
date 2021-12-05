a890fcd85a8b40dc1646754da1c14cc376949dbb
rmohanmanickam <cserajesh5@gmail.com>
2021-12-06 01:11:56 +0530

    Module

---
+++ prod/variables.tf
@@ -0,0 +1,15 @@
+variable "sgname" {
+  sensitive = true
+}
+
+variable "cidr" {
+  sensitive = true
+}
+
+variable "mytag" {}
+
+variable "amiid" {}
+
+variable "machinetype" {}
+
+variable "keyname" {}
